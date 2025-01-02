5a9593c0937614b9bf629c0391bea301af64d91f
chenyu.liu <chenyu.liu@singularity-ai.com>
2025-01-02 14:10:09 +0000

    add app_gradio

--- app_gradio.py
+++ app_gradio.py
@@ -33,34 +33,36 @@ def process(upload_file, checked_metric_names):
 
     return status, csv_file_path, html_file_path, png_file_path, json_file_path
 
-with gr.Blocks() as demo:
-    gr.Markdown(
-        """
-        <div style="text-align: center; font-size: 32px; font-weight: bold; margin-bottom: 20px;">
-        Image Analysis Tool
-        </div>  
-        """
-    )
-    upload_file = gr.File(label="upload file: 1. upload file which suffix in ['.csv', '.xlsx', '.txt', '.log'] 2. the format of each line must be either 'img_url' or 'img_url|type' 3. column titles are not required", file_types=[".csv", ".xlsx", ".txt", ".log"])
-    checked_metric_names = gr.CheckboxGroup(list(config.metric_params.keys()), label="metric_names", info="Check the metric names you want to detect")
-    process_button = gr.Button("Process")
-    status = gr.Textbox(label="Status", value="Processing not started", interactive=True)
-    with gr.Row():
-        csv_file = gr.File(label="Download csv file")
-        html_file = gr.File(label="Download html file")
-        png_file = gr.File(label="Download png file")
-        json_file = gr.File(label="Download json file")
+def get_demo():
+    with gr.Blocks() as demo:
+        gr.Markdown(
+            """
+            <div style="text-align: center; font-size: 32px; font-weight: bold; margin-bottom: 20px;">
+            Image Analysis Tool
+            </div>  
+            """
+        )
+        upload_file = gr.File(label="upload file: 1. upload file which suffix in ['.csv', '.xlsx', '.txt', '.log'] 2. the format of each line must be either 'img_url' or 'img_url|type' 3. column titles are not required", file_types=[".csv", ".xlsx", ".txt", ".log"])
+        checked_metric_names = gr.CheckboxGroup(list(config.metric_params.keys()), label="metric_names", info="Check the metric names you want to detect")
+        process_button = gr.Button("Process")
+        status = gr.Textbox(label="Status", value="Processing not started", interactive=True)
+        with gr.Row():
+            csv_file = gr.File(label="Download csv file")
+            html_file = gr.File(label="Download html file")
+            png_file = gr.File(label="Download png file")
+            json_file = gr.File(label="Download json file")
 
-        # csv_file = gr.DataFrame(label="CSV Preview")
-        # html_file = gr.HTML(label="HTML Preview")
-        # png_file = gr.Image(label="PNG Preview")
-        # json_file = gr.JSON(label="JSON Preview")
+            # csv_file = gr.DataFrame(label="CSV Preview")
+            # html_file = gr.HTML(label="HTML Preview")
+            # png_file = gr.Image(label="PNG Preview")
+            # json_file = gr.JSON(label="JSON Preview")
 
-    process_button.click(
-        process,
-        inputs=[upload_file, checked_metric_names],
-        outputs=[status, csv_file, html_file, png_file, json_file]
-    )
-    
+        process_button.click(
+            process,
+            inputs=[upload_file, checked_metric_names],
+            outputs=[status, csv_file, html_file, png_file, json_file]
+        )
+    return demo
+
 if __name__ == "__main__":
-    demo.launch(share=True)
+    get_demo().launch(share=True)
